d1ac396fc065678d67c78cf97bb96231a076e0fe
nagisa yata <kanbai.8ta.n@gmail.com>
2015-10-29 22:56:32 +0100

    fix date_of_week configuration

--- date_of_week.py
+++ date_of_week.py
@@ -2,14 +2,41 @@
 # 
 # 28.Oct.2015 Nagisa Yata
 
-from datetime import date
-from leapyear import detect_leapyear
-
+import datetime
+import math
 
 # define function
-def determine_day_of_week(date):
+def determine_day_of_week(date_original):
   weekday_str = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-  return weekday_str[date.weekday]
+
+  year = date_original.year
+  month = date_original.month
+  
+  # January and Feburary are operated 
+  # as 13 and 14 in previous year
+  if month < 3:
+    month += 12
+    year -= 1
+
+  date = datetime.date(year, month, date_original.day)
+
+  capital_y = date.year
+  
+  # Gregorian calendar
+  gamma = 6 * math.floor(capital_y/100) + math.floor(capital_y / 400)
+  
+  # Julian calendar
+  if date.year < 1582 and date.year > 4:
+    gamma = 5
+
+  # Zeller's congruence
+  # this congruence represent Saturday is 0
+  # then "-2" is used for number moved for index
+  weekday = (date.day + math.floor(26 * (date.month + 1) / 10) + capital_y \
+            + math.floor(capital_y / 4) + gamma) % 7 - 2
+  
+  return weekday_str[int(weekday)]
+    
 
 # start program
 date = raw_input("Input the date (DD/MM/YYYY):")
@@ -18,55 +45,28 @@ split_date = date.split("/")
 data_set = [2, 4, 6, 9, 11]
 
 i = 0
-year = 0
-month = 0
-day = 0
+y = 0
+m = 0
+d = 0
 for s in split_date:
   if not s.isdigit():
-    print "please input date"
+    print "1please input date"
     quit()
   elif i == 0:
-    if len(s) > 2:
-      print "please input date"
-      quit()
-    if int(s) < 1 or int(s) > 31:
-      print "please input date"
-      quit()
-    else:
-      day = int(s)
+    d = int(s)
   elif i == 1:
-    if len(s) > 2:
-      print "please input date"
-      quit()
-    if int(s) < 1 or int(s) > 12:
-      print "please input date"
-      quit()
-    if not s in data_list:
-      if day > 30:
-        print "please input date"
-        quit()
-    else:
-      month = int(s)
+    m = int(s)
   elif i == 2:
-    if detect_leapyear(s):
-      if month == 2:
-        if day > 29:
-          print "please input date"
-          quit()
-    else:
-      if month == 2:
-        if day > 28:
-          print "please input date"
-          quit()
-    year = int(s)
+    y = int(s)
   elif i > 2:
     print "please input date"
     quit()
+  i += 1
 
-if day == 0 or month == 0 or year == 0:
-  print "please input date"
+if d == 0 or m == 0 or y == 0:
+  print "2please input date"
   quit()
 
 # end validation
 
-print datermine_day_of_week(datetime.date(year, month, day))
+print determine_day_of_week(datetime.date(y,m,d))
